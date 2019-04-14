3cdb059f429fafcad8a2842009fec148681a4a91
your_username <your_email@example.com>
2019-04-14 14:17:41 +0000

    formata preparo

--- util.R
+++ util.R
@@ -45,27 +45,28 @@ cnt_seps_df <- function(fname,locale,sep,norm_cnt,max_rows=10)
   summarize(n=n(),row_list=str_c(head(row,max_rows),collapse=",")) %>%
   mutate(row_list=if_else(n>max_rows,str_c(row_list,",..."),row_list))
 
-trunc_cols <- function(fname,locale,max_cols,sep=";",n_max=-1L)
+trunc_cols <- function(fname,locale,max_cols,sep,n_max=-1L)
   read_lines(fname,locale=locale,n_max=n_max)%>%
   str_split(fixed(sep)) %>%
   map(str_squish)%>%
   map(head,max_cols) %>% # trunc
   map_chr(str_c,collapse=sep)
 
-trunc_file <- function(fname,locale,max_cols) {
+trunc_file <- function(fname,locale,max_cols,sep) {
   fname_utf8 <- get_fname_utf8(fname) 
-  trunc_cols(fname,locale,max_cols)%>%
+  trunc_cols(fname,locale,max_cols,sep)%>%
     write_lines(fname_utf8)
   fname_utf8
 }
 
-trunc_to_rds <- function(fname_txt,locale,max_cols) {
+trunc_to_rds <- function(fname_txt,locale,max_cols,sep) {
   # talvez nao seja necessario: tentar cols_only() em read_delim()
-  fname_utf8 <- trunc_file(fname_txt,locale,max_cols)
+  fname_utf8 <- trunc_file(fname_txt,locale,max_cols,sep)
   locale$encoding <- "UTF-8"
   df_fname <- read_delim(fname_utf8
                          ,locale=locale
-                         ,delim=fixed(";")
+                         ,delim=fixed(sep)
+                         ,trim_ws = T
                          ,quote="'"
                          #,col_types = cols(.default = "c")
                          ,col_types = cols(
@@ -79,8 +80,7 @@ trunc_to_rds <- function(fname_txt,locale,max_cols) {
                            Processo=col_character(),
                            Elemento=col_character(),
                            SubFuncao=col_character(),
-                           ContaCorrente=col_character()),
-                         
+                           ContaCorrente=col_character())
   )
   fname_rds <- fname_txt%>%repl_ext("rds") 
   df_fname %>% write_rds(fname_rds,compress="bz")
